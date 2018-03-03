b5eec1986f19e24fac140afeeb3aee30f63d08fb
Oli Bishop <oliverbishop90@gmail.com>
2018-03-03 17:55:02 +0000

    updated gitignore, added historic data functions

--- scraper.py
+++ scraper.py
@@ -103,6 +103,58 @@ def parse_ss_stats(soup):
 
     return pd.DataFrame(data, columns=['Placement Pts', 'Kills', 'Kill Pts', 'Overall Pts'])
 
+def parse_historic(soup):
+    schedule = soup.find_all('div', {'class': 'tournament-group-preview-holder mod-full mod-small-pd mod-pubg'})
+
+    results = []
+
+    #ignore total tournament standings by only considering [1:]
+    for game in schedule[1:]:
+        positions = []
+        team_names = []
+        for team_details in game.find_all('div', {'class': 'preview-item'}):
+            strings = team_details.find_all('span', limit=2)
+            positions.append(int(strings[0].text))
+            team_names.append(strings[1].text)
+
+        data = {
+            'Position': positions,
+            'Team Name': team_names
+        }
+
+        results.append(pd.DataFrame(data, columns=['Position', 'Team Name']))
+
+    return results
+
+def parse_historic_results(soup):
+    schedule = soup.find_all('div', {'class': 'tournament-group-preview-holder mod-full mod-small-pd mod-pubg'})
+
+    results = []
+    # team_list = soup.find('div', {'class': 'preview-item-list'})
+    # team_details = team_list.find_all('div', {'class': 'preview-item'})
+
+    for game in range(1,len(schedule)):
+        team_details = schedule[game].find_all('div', {'class': 'preview-item'})
+        placement_pts = []
+        kills = []
+        kills_pts = []
+        overall_pts = []
+        for team in team_details:
+            placement_pts.append(team.contents[5].find('span').text)
+            kills.append(team.contents[7].find('span').text)
+            kills_pts.append(team.contents[9].find('span').text)
+            overall_pts.append(team.contents[11].find('span').text)
+
+        data = {
+            'Placement Pts': placement_pts,
+            'Kills': kills,
+            'Kill Pts': kills_pts,
+            'Overall Pts': overall_pts
+        }
+
+        results.append(pd.DataFrame(data, columns=['Placement Pts', 'Kills', 'Kill Pts', 'Overall Pts']))
+    return results
+
 # Dictionary of websites to access
 urls = {
     # 'Web Scraper Test Site': 'http://webscraper.io',
@@ -115,10 +167,12 @@ for key, value in urls.items():
     print(value)
     try:
         page = BeautifulSoup(get_url(value).text, 'html.parser')
-        team_standings = parse_ss_teams(page)
-        team_stats = parse_ss_stats(page)
 
-        print(pd.concat([team_standings,team_stats], axis=1))
+        team_standings = parse_historic(page)
+        team_stats = parse_historic_results(page)
+
+        for game in range(0,len(team_standings)):
+            print(pd.concat([team_standings[game],team_stats[game]], axis=1))
 
     except custom_exception.DisallowedException as e:
         print('Connection to {} not permitted with HTTP code {}. Does its robots.txt allow access?'.format(value,
